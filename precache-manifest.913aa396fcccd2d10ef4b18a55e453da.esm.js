84bd4498decb65c9fede09faa13a4498b55dfb62
Phineas Jensen <phin@zayda.net>
2020-05-23 13:50:08 -0600

    Initial version

---
+++ precache-manifest.913aa396fcccd2d10ef4b18a55e453da.esm.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "891ffdf9d4758985ced0",
+    "url": "/bundle.5fa07.esm.js"
+  },
+  {
+    "revision": "891ffdf9d4758985ced0",
+    "url": "/bundle.917bf.css"
+  },
+  {
+    "revision": "90eacdfd6cc589a6bc7259e8b28438bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84c137f4ba19bae1d8d8",
+    "url": "/polyfills.989dd.esm.js"
+  },
+  {
+    "revision": "dee24afcc69e073f4247",
+    "url": "/route-home.chunk.1ffd2.esm.js"
+  },
+  {
+    "revision": "dee24afcc69e073f4247",
+    "url": "/route-home.chunk.e6c71.css"
+  },
+  {
+    "revision": "dc8dd792cbaeebd168d2",
+    "url": "/route-language-tools.chunk.ecf86.esm.js"
+  }
+]);
